87db1871dd029fbe65442c731cac7b2cc4767583
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2022-12-07 20:19:29 +0100

    Configuring the autofac

--- OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
-
+using OdeToFood.Data.Services;
 
 namespace OdeToFood.Web
 {
@@ -13,8 +14,11 @@ namespace OdeToFood.Web
         internal static void RegisterContainer()
         {
             var builder = new ContainerBuilder();
-            builder.RegisterControllers();
-
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<InMemoryRestaurantData>().As<IRestaurantData>();
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            
         }
     }
 }
