0ae394bd53e059d0b270fdac07cd4b2179b24c0d
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2022-12-21 21:55:59 +0100

    Working with enum

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -30,5 +30,9 @@ namespace OdeToFood.Web.Controllers
             }
             return View(model);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
